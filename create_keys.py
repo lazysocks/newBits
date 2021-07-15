eecc48083fb22abc923097f2916ebb16265a19de
Kevin McKinley <kevin.mckinley@lazysocks.net>
2021-07-15 10:13:43 -0700

    added populate_device dictionary func

--- create_keys.py
+++ create_keys.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python
-import psutil, re, subprocess, requests, os
+import psutil, re, subprocess, requests, os, collections
 from tqdm import tqdm
 from pathlib import Path
 
 
 
-#Configuration 
+#Configuration
 workdir = '/tmp/tmp.newbits'
 configurl = 'https://dl.google.com/dl/edgedl/chromeos/recovery/recovery.conf?source=linux_recovery.sh'
 ostoolversion='0.9.2'
@@ -14,11 +14,17 @@ ostoolversion='0.9.2'
 config='config.txt'
 
 #Helper Functions
+
+def makehash():
+    return collections.defaultdict(makehash)
+
+#Cleanup files
 def cleanup(path, filename):
     file_path = os.path.join(path, filename)
     if os.path.exists(file_path):
         os.remove(file_path)
 
+#Retrieve file from web
 def get_file(url, filename,resume=False):
     file_path = os.path.join(workdir,filename)
     if resume is False:
@@ -46,9 +52,28 @@ def get_file(url, filename,resume=False):
                     if chunk:
                         data.write(chunk)
                         pbar.update(len(chunk))
-
-    
-        
+#Populate devices from config into dictionary
+def populate_devices(dict):
+    config_file = os.path.join(workdir, config)
+    pattern = re.compile(r'(name=)')
+    with open(config_file, 'r') as data:
+        lines = data.readlines()
+        for i, line in enumerate(lines):
+            if re.search(pattern, line):
+                device = line.strip()
+                device = line.strip('name=')
+                version = lines[i+1].strip('version=')
+                desc = lines[i+2].strip('desc=')
+                channel = lines[i+3].strip('channel=')
+                hwidmatch = lines[i+4].strip('hwidmatch=')
+                hwid = lines[i+5].strip('hwid=')
+                md5 = lines[i+6].strip('md5=')
+                sha1 = lines[i+7].strip('sha1=')
+                zipfilesize = lines[i+8].strip('zipfilesize=')
+                filename = lines[i+9].strip('file=')
+                filesize = lines[i+10].strip('filesize=')
+                url = lines[i+11].strip('url=')
+                dict[device] = { 'version': version.strip(), 'desc': desc.strip(), 'channel': channel.strip(),'hwidmatch': hwidmatch.strip(), 'hwid': hwid.strip(), 'md5': md5.strip(), 'sha1': sha1.strip(), 'zipfilesize': zipfilesize.strip(), 'file': filename.strip(), 'filesize': filesize.strip(), 'url': url.strip() }
 
 
 #Get All Drives SCSI devices, even the ones we don't want
@@ -70,7 +95,7 @@ def get_drives():
 def get_usbdrives(drives):
     usb_drives = []
     for drive in drives:
-        
+
         drive_type = subprocess.run(['cat', f'/sys/block/{drive}/device/type'], capture_output=True, text=True).stdout.strip()
         drive_removable = subprocess.run(['cat', f'/sys/block/{drive}/removable'], capture_output=True, text=True).stdout.strip()
         drive_link = subprocess.run(['readlink', '-f', f'/sys/block/{drive}'], capture_output=True, text=True).stdout.strip()
@@ -106,4 +131,4 @@ else:
 testurl = 'https://dl.google.com/dl/edgedl/chromeos/recovery/chromeos_13904.55.0_asuka_recovery_stable-channel_mp.bin.zip'
 get_file(configurl, config)
 get_file(testurl, 'test.zip', resume=True)
-          
+
