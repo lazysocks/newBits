497c333dab861bbe463cac86a9f1b4d7d1f27e5d
Kevin McKinley <km@lazysocks.net>
2023-04-10 10:09:02 -0700

    renamed path to filedir

--- recovery.py
+++ recovery.py
@@ -40,25 +40,25 @@ def run_recovery():
         #check for diskspace
         disks.check_disk_space(int(device.choice['filesize']), settings.workdir)
         #set paths for image files
-        path = settings.device_image_paths(device.choice)
-        if os.path.exists(path['zipfile']):
+        filedir = settings.device_image_paths(device.choice)
+        if os.path.exists(filedir['zipfile']):
             if Path(file_path).stat().st_size == device.choice['zipfilesize']:
                 print('File already exists, skipping download!')
             else:
-                utility.get_file(device.choice['url'], device.choice['file'] + '.zip', path['zipfile'], resume=True)
+                utility.get_file(device.choice['url'], device.choice['file'] + '.zip', filedir['zipfile'], resume=True)
         else:
-            utility.get_file(device.choice['url'], device.choice['file'] + '.zip', path['zipfile'])
+            utility.get_file(device.choice['url'], device.choice['file'] + '.zip', filedir['zipfile'])
         #check file integrity
-        if utility.check_sha1(path['zipfile'], device.choice['sha1']):
+        if utility.check_sha1(filedir['zipfile'], device.choice['sha1']):
             pass
         else:
             print('Image did not pass SHA1 hash check..exiting')
             exit()
         #time to unpack image
-        utility.unpack_zipfile(path['zipfile'], path['imagefile'], device.choice['filesize'])
+        utility.unpack_zipfile(filedir['zipfile'], filedir['imagefile'], device.choice['filesize'])
 
         #time to apply image
-        utility.apply_image(path['imagefile'], usb_keys.removable)
+        utility.apply_image(filedir['imagefile'], usb_keys.removable)
     else:
         print("Aborting program...")
         exit()
